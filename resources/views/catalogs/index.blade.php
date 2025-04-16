9de1dc7d6abbd767ca18a8c524e01e193a734216
d0m0n <d0m0n_wz7@live.jp>
2025-04-16 12:17:46 +0900

    ボタンのスタイルを微修正

--- resources/views/catalogs/index.blade.php
+++ resources/views/catalogs/index.blade.php
@@ -11,7 +11,7 @@
                     <div class="flex items-center space-x-4 mb-4">
                         <form method="GET" action="{{ route('catalogs.index') }}" id="filterForm" class="flex items-center space-x-4">
                             <div>
-                                <select name="country_name" id="country_name" class="mt-4 block w-full border-gray-300 rounded-md shadow-sm text-sm" onchange="document.getElementById('filterForm').submit();">
+                                <select name="country_name" id="country_name" class="mt-4 block w-full border-white rounded-md shadow-sm text-sm" onchange="document.getElementById('filterForm').submit();">
                                     <option value="">国名を選択</option>
                                     @foreach ($countries as $country)
                                         <option value="{{ $country }}" {{ request('country_name') == $country ? 'selected' : '' }}>
@@ -21,7 +21,7 @@
                                 </select>
                             </div>
                             <div>
-                                <select name="location_name" id="location_name" class="mt-4 block w-full border-gray-300 rounded-md shadow-sm text-sm" onchange="document.getElementById('filterForm').submit();">
+                                <select name="location_name" id="location_name" class="mt-4 block w-full border-white rounded-md shadow-sm text-sm" onchange="document.getElementById('filterForm').submit();">
                                     <option value="">地域名を選択</option>
                                     @foreach ($locations as $location)
                                         <option value="{{ $location }}" {{ request('location_name') == $location ? 'selected' : '' }}>
@@ -31,7 +31,7 @@
                                 </select>
                             </div>
                             <div>
-                                <select name="status_id" id="status_id" class="mt-4 block w-full border-gray-300 rounded-md shadow-sm text-sm" onchange="document.getElementById('filterForm').submit();">
+                                <select name="status_id" id="status_id" class="mt-4 block w-full border-white rounded-md shadow-sm text-sm" onchange="document.getElementById('filterForm').submit();">
                                     <option value="">在留資格を選択</option>
                                     @foreach ($statuses as $status)
                                         <option value="{{ $status->id }}" {{ request('status_id') == $status->id ? 'selected' : '' }}>
@@ -84,27 +84,26 @@
                                     </div>
                                 @endif
                                 <div class="flex items-center justify-between text-gray-700 mt-2">
-                                    <p class="text-2xl font-bold">{{ $catalog->copy }}</p>
+                                    <a href="{{ route('catalogs.show', $catalog) }}" class="text-2xl hover:text-emerald-500 font-bold">{{ $catalog->copy }}</a>
                                     <div class="flex items-center space-x-4">
                                         <p class="text-l font-bold"><strong>初期費用</strong></p>
                                         <p class="text-2xl font-bold">{{ number_format($catalog->price) }} 円</p>
-                                        <a href="{{ route('catalogs.show', $catalog) }}" class="px-6 py-2 text-white rounded-md bg-gradient-to-r from-emerald-500 to-sky-600 hover:from-emerald-300 hover:to-sky-400 hover:shadow-sm ">
+                                        <a href="{{ route('catalogs.show', $catalog) }}" class="px-6 py-2 text-white rounded-md bg-gradient-to-r from-emerald-500 to-teal-500 hover:from-emerald-700 hover:to-teal-700 hover:shadow-sm ">
                                             詳細はこちら
                                         </a>
                                     </div>
                                 </div>
                                 <hr class="border-gray-300 my-2">
                                 <span class="text-sm text-gray-700 font-semibold">
-                                    <strong>提供:</strong> 
                                     <a href="{{ route('catalogs.provider.list', ['provider_id' => $catalog->provider_id]) }}" class="text-blue-500 hover:text-blue-700">
-                                        {{ $catalog->provider->name ?? '不明' }}
+                                        {{ $catalog->provider->name ?? '未登録' }}
                                     </a>
                                 </span>
-                                <span class="text-xs text-gray-700">所在地:{{ $catalog->provider->location ?? '不明' }}
+                                <span class="text-xs text-gray-700">所在地:{{ $catalog->provider->location ?? '未登録' }}
                                 </span>
-                                <span class="text-xs text-gray-700">TEL.{{ $catalog->provider->phone_number ?? '不明' }}
+                                <span class="text-xs text-gray-700">TEL.{{ $catalog->provider->phone_number ?? '未登録' }}
                                 </span>
-                                <span class="text-xs text-gray-700">FAX.{{ $catalog->provider->fax_number ?? '不明' }}
+                                <span class="text-xs text-gray-700">FAX.{{ $catalog->provider->fax_number ?? '未登録' }}
                                 </span>
                             </div>
                         @endforeach
